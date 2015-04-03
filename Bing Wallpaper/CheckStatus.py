9b3bba3cdad7162f29db6519c9bcf7dddde2ef38
Bobin Joseph <bobinjoseph@Bobins-MacBook-Pro.local>
2015-04-02 18:04:34 -0700

    Removed functions, they seemed to cause issues. Also updated the app names in the script

--- Bing Wallpaper/CheckStatus.py
+++ Bing Wallpaper/CheckStatus.py
@@ -10,56 +10,55 @@ except:
 #Get home folder
 homeFolder = os.path.expanduser("~")
 pictureLocation = homeFolder + "/Pictures/bing-wallpaper"
-appLocation = homeFolder + "/Applications/Bing\ Wallpaper/Bing.app"
+appLocation = homeFolder + "/Applications/Bing\ Wallpaper/BingWallpaper.app"
 internetConnected = False
-def checkInternetStatus():
-    #Check to see if there is an active internet connection
-    try:
-        urlopen("http://www.bing.com",timeout=10)
-        internetConnected = True
-    except:
-        internetConnected = False
 
-def processWallpaper():
-    #Flag to see if any jpg file was found in the bing-wallpaper folder
-    fileExists=0
+#Check to see if there is an active internet connection
+try:
+    urlopen("http://www.bing.com",timeout=10)
+    internetConnected = True
+except:
+    internetConnected = False
 
-    #Check to see if the bing-wallpaper folder exists in the Pictures directory
-    if not os.path.exists(pictureLocation):
-        #If it does not exist, create the directory
-        os.makedirs(pictureLocation)
-    #Find the jpg file in the bing-wallpaper folder
-    for f in os.listdir(pictureLocation):
-        #if there is a file...
-        if os.path.isfile(pictureLocation + f):
-            #Check to see if the file ends with jpg. This removes the .DS files
-            if f.endswith(".jpg"):
-                #Flag that a jpg file was found
-                fileExists=1
-                file = pictureLocation + f
-                #Get change date of the file
-                #Returns number of seconds since the epoch
-                fileChange = os.path.getctime(file)
 
-                #Convert change time into string
-                changeTimeStr = (datetime.datetime.fromtimestamp(fileChange).strftime('%Y-%m-%d %H:%M:%S.%f'))
+#Flag to see if any jpg file was found in the bing-wallpaper folder
+fileExists=0
 
-                #Convert the
-                changeTime = datetime.datetime.strptime(changeTimeStr, "%Y-%m-%d %H:%M:%S.%f")
+#Check to see if the bing-wallpaper folder exists in the Pictures directory
+if not os.path.exists(pictureLocation):
+    #If it does not exist, create the directory
+    os.makedirs(pictureLocation)
+#Find the jpg file in the bing-wallpaper folder
+for f in os.listdir(pictureLocation):
+    #if there is a file...
+    if os.path.isfile(pictureLocation + f):
+        #Check to see if the file ends with jpg. This removes the .DS files
+        if f.endswith(".jpg"):
+            #Flag that a jpg file was found
+            fileExists=1
+            file = pictureLocation + f
+            #Get change date of the file
+            #Returns number of seconds since the epoch
+            fileChange = os.path.getctime(file)
 
-                #Get the time now
-                today = datetime.datetime.today()
+            #Convert change time into string
+            changeTimeStr = (datetime.datetime.fromtimestamp(fileChange).strftime('%Y-%m-%d %H:%M:%S.%f'))
 
-                #Get the difference between now and the date the file was changed
-                c = today.date() - changeTime.date()
+            #Convert the
+            changeTime = datetime.datetime.strptime(changeTimeStr, "%Y-%m-%d %H:%M:%S.%f")
 
-                #If the difference is 1 day, then run the app to download a new image, else pass.
-                if c.days > 0 and internetConnected==True:
-                    os.system("open " + appLocation)
+            #Get the time now
+            today = datetime.datetime.today()
 
-    #If there was no jpg file in the folder, run the app to get a new wallpaper
-    if internetConnected==True and fileExists==0:
-        os.system("open " + appLocation)
+            #Get the difference between now and the date the file was changed
+            c = today.date() - changeTime.date()
 
-checkInternetStatus()
-processWallpaper()
+            #If the difference is 1 day, then run the app to download a new image, else pass.
+            if c.days > 0 and internetConnected==True:
+                os.system("open " + appLocation)
+
+#If there was no jpg file in the folder, run the app to get a new wallpaper
+if internetConnected==True and fileExists==0:
+    os.system("open " + appLocation)
+
+
