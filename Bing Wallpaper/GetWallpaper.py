ccb77f341da604db33d2b9a4811796d9e25da301
Bobin Joseph <bobinjoseph@Bobins-MacBook-Pro.local>
2015-04-10 09:30:50 -0700

    Modified url to get the high res 1900x1200 image
    The flag getHighRes is set to 1 by default to get the highest
    resolution image. This can be changed to 0 if the script breaks

--- Bing Wallpaper/GetWallpaper.py
+++ Bing Wallpaper/GetWallpaper.py
@@ -1,6 +1,7 @@
 import json
 #URL in json format for latest wallpaper
 url = "http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US"
+getHighRes = 1 #Manually change the resolution in the url to 1920x1200. Change to 0 if url breaks.
 
 #Try with python3
 try:
@@ -9,7 +10,11 @@ try:
     output = response.readall().decode('utf-8')
     data = json.loads(output)
     output_url = "http://www.bing.com/" + data["images"][0]["url"]
-    print(output_url) #Prints url to the image. Passes this on to the Automator workflow
+    if output_url:
+        if getHighRes == 1:
+            print(output_url.replace("1080", "1200")) #Prints url to the image. Passes this on to the Automator workflow
+        else:
+            print(output_url) #Prints url to the image. Passes this on to the Automator workflow
 
 #Else use python2
 except:
@@ -18,5 +23,8 @@ except:
     output = response.read()
     data = json.loads(output)
     output_url = "http://www.bing.com/" + data["images"][0]["url"]
-    print(output_url) #Prints url to the image. Passes this on to the Automator workflow
-
+    if output_url:
+        if getHighRes == 1:
+            print(output_url.replace("1080", "1200")) #Prints url to the image. Passes this on to the Automator workflow
+        else:
+            print(output_url) #Prints url to the image. Passes this on to the Automator workflow
