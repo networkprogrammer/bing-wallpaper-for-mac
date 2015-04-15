be6bc5faee40959bf014fcf99625923d5ba2db40
Bobin Joseph <bobinjoseph@Bobins-MBP.socal.rr.com>
2015-04-14 17:57:44 -0700

    Cleaned up python and automator app
    Python script now downloads to ~/Downloads and automator app will move
    the image file to bing-wallpaper location

--- Bing Wallpaper/GetWallpaper.py
+++ Bing Wallpaper/GetWallpaper.py
@@ -1,42 +1,77 @@
 import json
-#URL in json format for latest wallpaper
-url = "http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US"
-getHighRes = 1 #Manually change the resolution in the url to 1920x1200. Change to 0 if url breaks.
 
 #Try with python3
 try:
-    from urllib.request import urlopen
-    response = urlopen(url)
-    output = response.readall().decode('utf-8')
-    data = json.loads(output)
-    output_url = "http://www.bing.com/" + data["images"][0]["url"]
-    if output_url:
-        if getHighRes == 1:
-            try:
-                from urllib.request import urlretrieve
-                req = urlretrieve(output_url.replace("1080", "1200"), "001.jpg")
-                print(output_url.replace("1080", "1200")) #Prints url to the image. Passes this on to the Automator workflow
-            except:
-                print(output_url)
-        else:
-            print(output_url) #Prints url to the image. Passes this on to the Automator workflow
+    from urllib.request import urlopen, urlretrieve
+    from urllib.request import urlretrieve
 
-
-
-#Else use python2
+#Else try python2
 except:
     from urllib2 import urlopen
+    from urllib import urlretrieve
+
+from os import path
+
+
+#User home folder
+homeFolder = path.expanduser("~")
+
+#Save pictures to a folder
+pictureLocation = homeFolder + "/Downloads/"
+
+
+def main():
+    ########Defining variables#######
+
+    #URL in json format for latest wallpaper
+    url = "http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US"
+    getHighRes = 1 #Manually change the resolution in the url to 1920x1200. Change to 0 if url breaks.
+
+    #Get json response from bing.com
     response = urlopen(url)
-    output = response.read()
+
+    #Trying python 3
+    try:
+        output = response.readall().decode('utf-8')
+
+    #Else trying python2
+    except:
+           output = response.read()
+    #Get json output
     data = json.loads(output)
+
+    #Form image url from json
     output_url = "http://www.bing.com/" + data["images"][0]["url"]
-    if output_url:
-        if getHighRes == 1:
-            try:
-                from urllib2.request import urlretrieve
-                req = urlretrieve(output_url.replace("1080", "1200"), "001.jpg")
-                print(output_url.replace("1080", "1200")) #Prints url to the image. Passes this on to the Automator workflow
-            except:
-                print(output_url)
-        else:
-            print(output_url) #Prints url to the image. Passes this on to the Automator workflow
+
+    #Form 1920x1200 image from above url
+    output_url_highres = output_url.replace("1080", "1200")
+
+
+    #If higher resolution is preferred(default)
+    if getHighRes == 1:
+        #Use try block to catch any failure in getting the high res image
+        try:
+            process_url(output_url_highres)
+
+        except:
+            process_url(output_url)
+
+    else:
+        process_url(output_url)
+
+
+def process_url(image_url):
+    #Get the filename of the new file from the url
+    filename = pictureLocation + image_url.split('/')[-1]
+
+    #Retrieve the image from the web and save it to desired location
+    req = urlretrieve(image_url, filename)
+
+    #Save the file path + filename to the output variable
+    bingImage = path.abspath(filename)
+    print(bingImage)
+
+
+
+
+main()
